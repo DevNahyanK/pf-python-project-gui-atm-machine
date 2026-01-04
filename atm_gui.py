9ffdfbf0fb0c1319fc503876d92cfaa7c542aee2
Muhammad Nahyan Khan <nahyangaming789@gmail.com>
2026-01-04 14:12:49 +0500

    ✔️

--- atm_gui.py
+++ atm_gui.py
@@ -2,73 +2,77 @@ from tkinter import *
 from tkinter import messagebox
 import os
 
-BALANCE_FILE = "balance.txt"
+files = {
+    "1111": "nahyhan-user1.txt",   
+    "2222": "kazim-user2.txt"
+}
 
-def load_balance():
-    if not os.path.exists(BALANCE_FILE):
-        with open(BALANCE_FILE, "w") as f:
-            f.write("1000")
-        return 1000
+balance = 0
+current_file = ""
 
-    with open(BALANCE_FILE, "r") as f:
-        data = f.read().strip()
-        if data == "":
-            return 1000
-        return float(data)
 
-# Save Balance
-def save_balance(amount):
-    with open(BALANCE_FILE, "w") as f:
-        f.write(str(amount))
+def load(file):
+    if not os.path.exists(file):
+        open(file, "w").write("1000")
+    return float(open(file).read())
 
-balance = load_balance()
+def save(file, amt):
+    open(file, "w").write(str(amt))
 
-# GUI
+# gui
 root = Tk()
-root.title("Simple ATM Machine")
-root.geometry("400x350")
-root.resizable(False, False)
+root.title("ATM (2 Users)")
+root.geometry("350x400")
 
-Label(root, text="ATM MACHINE", font=("Arial", 18, "bold")).pack(pady=10)
+Label(root, text="ATM MACHINE", font=("Arial", 16, "bold")).pack(pady=10)
 
-# Now use Function
-def check_balance():
-    messagebox.showinfo("Balance", f"Your balance is Rs {balance}")
+# login k liye
+def login():
+    global balance, current_file
+    pin = pin_entry.get()
+
+    if pin in files:
+        current_file = files[pin]
+        balance = load(current_file)
+        messagebox.showinfo("Login", "Login Successful")
+    else:
+        messagebox.showerror("Error", "Wrong PIN")
+
+Label(root, text="Enter PIN").pack()
+pin_entry = Entry(root, show="*")
+pin_entry.pack()
+
+Button(root, text="Login", command=login).pack(pady=10)
+
+# functions
+def show_balance():
+    messagebox.showinfo("Balance", f"Rs {balance}")
 
 def deposit():
     global balance
-    try:
-        amt = float(amount_entry.get())
-        if amt <= 0:
-            raise ValueError
-        balance += amt
-        save_balance(balance)
-        messagebox.showinfo("Success", "Amount Deposited")
-        amount_entry.delete(0, END)
-    except:
-        messagebox.showerror("Error", "Enter valid amount")
+    amt = float(amount.get())
+    balance += amt
+    save(current_file, balance)
+    messagebox.showinfo("Done", "Amount Deposited")
 
 def withdraw():
     global balance
-    try:
-        amt = float(amount_entry.get())
-        if amt <= 0 or amt > balance:
-            raise ValueError
+    amt = float(amount.get())
+    if amt > balance:
+        messagebox.showerror("Error", "Insufficient Balance")
+    else:
         balance -= amt
-        save_balance(balance)
-        messagebox.showinfo("Success", "Please collect your cash")
-        amount_entry.delete(0, END)
-    except:
-        messagebox.showerror("Error", "Invalid or insufficient balance")
+        save(current_file, balance)
+        messagebox.showinfo("Done", "Collect Cash")
 
-# ye user sai input k liye
+# user sai input k liye
 Label(root, text="Enter Amount").pack()
-amount_entry = Entry(root)
-amount_entry.pack(pady=5)
+amount = Entry(root)
+amount.pack()
 
-# button
-Button(root, text="Check Balance", width=25, command=check_balance).pack(pady=5)
-Button(root, text="Deposit", width=25, command=deposit).pack(pady=5)
-Button(root, text="Withdraw", width=25, command=withdraw).pack(pady=5)
-# run
+# BUTTONS
+Button(root, text="Check Balance", width=20, command=show_balance).pack(pady=5)
+Button(root, text="Deposit", width=20, command=deposit).pack(pady=5)
+Button(root, text="Withdraw", width=20, command=withdraw).pack(pady=5)
+
 root.mainloop()
